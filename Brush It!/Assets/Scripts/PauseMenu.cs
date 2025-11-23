25198f517386a6cb1835e880fc97731a5b66ae0c
SlymyCode <slymedox200@gmail.com>
2025-11-23 17:22:56 -0300

    Update 5

--- Brush It!/Assets/Scripts/PauseMenu.cs
+++ Brush It!/Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,13 @@
-using NUnit.Framework.Constraints;
 using UnityEngine;
-using TMPro;
-using Unity.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject playerUI;
+    [SerializeField] private GameObject timer;
     [SerializeField] private Key key = Key.Escape;
     private bool showMenu = false;
     
@@ -25,21 +26,51 @@ public class PauseMenu : MonoBehaviour
         if (Keyboard.current[key].wasPressedThisFrame && !showMenu)
         {
             pauseMenu.SetActive(true);
+            playerUI.SetActive(false);
+            timer.SetActive(false);
             showMenu = true;
             Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            
+
         }
         else if (Keyboard.current[key].wasPressedThisFrame && showMenu) 
         {
             pauseMenu.SetActive(false);
+            playerUI.SetActive(true);
+            timer.SetActive(true);
             showMenu = false;
             Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
 
-    public void Test()
+    public void Unpause()
     {
-        Debug.Log("Test");
+        pauseMenu.SetActive(false);
+        playerUI.SetActive(true);
+        timer.SetActive(true);
+        showMenu = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OpenOptionsMenu()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(true);
     }
 }
